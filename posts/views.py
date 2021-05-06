b0faedc01cc1dc7749b4d7281c72ecba16f51077
Dmytro-Sk <0971898920d@gmail.com>
2021-05-06 20:05:48 +0300

    Create pages - create .html files - create .css file - add pages urls

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
-# Create your views here.
+posts = [
+    {'author': 'Corey',
+     'title': 'Title1',
+     'date': '20.05',
+     },
+    {'author': 'Schafer',
+     'title': 'Title2',
+     'date': '21.06',
+     },
+]
+
+
+def home(request):
+    context = {'post': posts}
+    return render(request, 'posts/home.html', context)
+
+
+def about(request):
+    return render(request, 'posts/about.html')
