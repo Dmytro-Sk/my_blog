2cc1ddf0dd0a48c90c4c7b0f3239866f955a42a2
Dmytro-Sk <0971898920d@gmail.com>
2021-05-23 21:20:11 +0300

    Pagination and author posts review - add pagination - add UserPostListView - create template - add url - activate buttons - change indents .html files

--- posts/urls.py
+++ posts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, UserPostListView
 from . import views
 
 
@@ -9,7 +9,8 @@ urlpatterns = [
     path('', PostListView.as_view(), name='home'),
     path('post/new/', PostCreateView.as_view(), name='create'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='detail'),
-    path('post/<int:pk>/update', PostUpdateView.as_view(), name='update'),
-    path('post/<int:pk>/delete', PostDeleteView.as_view(), name='delete'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='delete'),
+    path('user/<str:username>/', UserPostListView.as_view(), name='user-posts'),
     path('about/', views.about, name='about'),
 ]
