0f1a066bf3998734fa77d8d97a8ef02caf73f699
Dmytro-Sk <0971898920d@gmail.com>
2021-05-23 16:21:25 +0300

    Create, update, delete posts - add list, create, detail, update, delete classes - add urls - create templates - add/activate buttons - add conditions for a possibility to update or delete posts

--- posts/urls.py
+++ posts/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
+
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 from . import views
 
 
 app_name = 'posts'
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('about', views.about, name='about'),
+    path('', PostListView.as_view(), name='home'),
+    path('post/new/', PostCreateView.as_view(), name='create'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='detail'),
+    path('post/<int:pk>/update', PostUpdateView.as_view(), name='update'),
+    path('post/<int:pk>/delete', PostDeleteView.as_view(), name='delete'),
+    path('about/', views.about, name='about'),
 ]
